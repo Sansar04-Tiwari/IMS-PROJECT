eaec7a0710f00a2140e65d9f9a699234cb65bd3a
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-06-12 15:43:29 +0530

    Routing updated

--- inventory management system/src/App.jsx
+++ inventory management system/src/App.jsx
@@ -2,41 +2,26 @@ import './App.css'
 
 import Sidebar from './components/siderbar/Sidebar'
 import Header from './components/Header/Header'
-import  Footer from './components/Footer/Footer'
-import  AddProduct from './components/Product/AddProduct'
-import  ListProduct from './components/Product/ListProduct'
+import Footer from './components/Footer/Footer'
+import AddProduct from './components/Product/AddProduct'
+import ListProduct from './components/Product/ListProduct'
 import Dashboard from './components/Dashboard/Dashboard'
-
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-
-
-
 
 function App() {
 
   return (
 
-<div className="wrapper">
-
-<Sidebar/>
-<Header/>
-<BrowserRouter>
-<Routes>
-  
-<Route path='/' element={<Dashboard/>}/>
-<Route path='/addproduct' element={<AddProduct/>}/>
-<Route path='/listproduct' element={<ListProduct/>}/>
-
-        
-</Routes> 
-</BrowserRouter>
-<Footer/>
-
-
-   
-    
+    <div className="wrapper">
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <Routes >
+          <Route path='/' element={<Dashboard />} />
+          <Route path='listProduct' element={<ListProduct/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
